18c5afdff460f737e455ce9f71176acdc9d7fad5
Sven Kreiss <me@svenkreiss.com>
2018-08-17 10:13:46 +0200

    fix V0 for reference MLP

--- tests/fit_pedped.py
+++ tests/fit_pedped.py
@@ -1,8 +1,42 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import scipy.optimize
 import socialforce
+
+
+def visualize(file_prefix, V, initial_parameters, fit_result):
+    b = np.linspace(0, 3)
+    y_ref = 2.1 * np.exp(-1.0 * b / 0.3)
+    V.set_parameters(torch.tensor(initial_parameters))
+    y_initial = V.value_b(torch.from_numpy(b).float()).detach().numpy()
+    V.set_parameters(torch.tensor(fit_result.x))
+    y_mlp = V.value_b(torch.from_numpy(b).float()).detach().numpy()
+
+    with socialforce.show.graph(file_prefix + 'v.png') as ax:
+        ax.set_xlabel('$b$ [m]')
+        ax.set_ylabel('$V$')
+        ax.plot(b, y_ref, label=r'$V_0 e^{-b/\sigma}$', color='C0')
+        ax.axvline(0.3, color='black', linestyle='dotted', label=r'$\sigma$')
+        ax.plot(b, y_initial, label=r'untrained MLP($b$)',
+                linestyle='dashed', color='black')
+        ax.plot(b, y_mlp, label=r'MLP($b$)', color='orange')
+        ax.legend()
+
+    with socialforce.show.graph(file_prefix + 'gradv.png') as ax:
+        ax.set_xlabel(r'$b$ [m]')
+        ax.set_ylabel(r'$\nabla V$')
+        delta_b = b[1:] - b[:-1]
+        average_b = 0.5 * (b[:-1] + b[1:])
+        ax.plot(average_b, (y_initial[1:] - y_initial[:-1]) / delta_b,
+                label=r'untrained MLP($b$)',
+                linestyle='dashed', color='black')
+        ax.plot(average_b, (y_mlp[1:] - y_mlp[:-1]) / delta_b,
+                label=r'MLP($b$)', color='orange')
+        ax.plot(average_b, (y_ref[1:] - y_ref[:-1]) / delta_b,
+                label=r'$V_0 e^{-b/\sigma}$', color='C0')
+        ax.axvline(0.3, color='black', linestyle='dotted', label=r'$\sigma$')
+        ax.set_ylim(-4.9, 0.5)
+        ax.legend()
 
 
 def test_opposing():
@@ -28,7 +62,7 @@ def test_opposing():
         return loss
 
     parameters = np.array([1.2, 0.1])
-    res = scipy.optimize.minimize(f, parameters, method='BFGS', options={'eps': 1e-3})
+    res = scipy.optimize.minimize(f, parameters, method='L-BFGS-B', options={'eps': 1e-4})
     print(res)
 
 
@@ -68,35 +102,4 @@ def test_opposing_mlp():
     print(res)
 
     # make plots of result
-    b = np.linspace(0, 3)
-    y_ref = 2.3 * np.exp(-1.0 * b / 0.3)
-    V.set_parameters(torch.tensor(initial_parameters))
-    y_initial = V.value_b(torch.from_numpy(b).float()).detach().numpy()
-    V.set_parameters(torch.tensor(res.x))
-    y_mlp = V.value_b(torch.from_numpy(b).float()).detach().numpy()
-
-    with socialforce.show.graph('docs/mlp_v.png') as ax:
-        ax.set_xlabel('$b$ [m]')
-        ax.set_ylabel('$V$')
-        ax.plot(b, y_ref, label=r'$V_0 e^{-b/\sigma}$', color='C0')
-        ax.axvline(0.3, color='black', linestyle='dotted', label=r'$\sigma$')
-        ax.plot(b, y_initial, label=r'untrained MLP($b$)',
-                linestyle='dashed', color='black')
-        ax.plot(b, y_mlp, label=r'MLP($b$)', color='orange')
-        ax.legend()
-
-    with socialforce.show.graph('docs/mlp_gradv.png') as ax:
-        ax.set_xlabel(r'$b$ [m]')
-        ax.set_ylabel(r'$\nabla V$')
-        delta_b = b[1:] - b[:-1]
-        average_b = 0.5 * (b[:-1] + b[1:])
-        ax.plot(average_b, (y_initial[1:] - y_initial[:-1]) / delta_b,
-                label=r'untrained MLP($b$)',
-                linestyle='dashed', color='black')
-        ax.plot(average_b, (y_mlp[1:] - y_mlp[:-1]) / delta_b,
-                label=r'MLP($b$)', color='orange')
-        ax.plot(average_b, (y_ref[1:] - y_ref[:-1]) / delta_b,
-                label=r'$V_0 e^{-b/\sigma}$', color='C0')
-        ax.axvline(0.3, color='black', linestyle='dotted', label=r'$\sigma$')
-        ax.set_ylim(-4.9, 0.5)
-        ax.legend()
+    visualize('docs/mlp_', V, initial_parameters, res)
