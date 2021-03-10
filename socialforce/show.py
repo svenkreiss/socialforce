cd0fdaa63c3010cdb3a69d4031d10f348b2a4a9c
Sven Kreiss <me@svenkreiss.com>
2021-03-10 13:10:46 +0100

    2d

--- socialforce/show.py
+++ socialforce/show.py
@@ -121,3 +121,66 @@ def potential1D_parametric(V, ax1, ax2=None, label=None, sigma_label=None, lines
     ax1.axvline(V.sigma, linestyle='dotted', label=sigma_label, **kwargs)
     if ax2 is not None:
         ax2.axvline(V.sigma, linestyle='dotted', label=sigma_label, **kwargs)
+
+
+def potential2D(V, ax, nx=601, ny=601, **kwargs):
+    # the "pedestrian of interest" is beta and the probe pedestrians are alpha
+
+    x1 = np.linspace(-0.5, 1.5, nx, dtype=np.float32)
+    x2 = np.linspace(-0.75, 0.75, ny, dtype=np.float32)
+    xx1, xx2 = np.meshgrid(x1, x2)
+    r_ab_probe = torch.from_numpy(np.stack((xx1, xx2), axis=-1)).view(-1, 2).unsqueeze(1)
+
+    speeds_b = torch.ones((r_ab_probe.shape[1],)) * 1.0
+    desired_directions_b = torch.zeros((r_ab_probe.shape[1], 2))
+    desired_directions_b[:, 0] = 1.0
+
+    ax.axhline(0.0, ls='dotted', color='gray')
+    ax.axvline(0.0, ls='dotted', color='gray')
+
+    with torch.no_grad():
+        values = V.value_r_ab(r_ab_probe, speeds_b, desired_directions_b, 1.0)
+        values -= torch.min(values)
+    values = values.view((len(x1), len(x2)))
+
+    ax.clabel(
+        ax.contour(x1, x2, values, levels=np.linspace(0.2, 3.0, 15), vmax=1.0),
+        inline=1, fontsize=10)
+
+    ax.plot([0.0], [0.0], 'o', label='pedestrian', markersize=5.0, color='black')
+    ax.set_xlabel('$x_1$ [m]')
+    ax.set_ylabel('$x_2$ [m]')
+    ax.set_aspect('equal')
+    ax.legend()
+
+
+def potential2Dgrad(V, ax, nx=601, ny=601, **kwargs):
+    # the "pedestrian of interest" is beta and the probe pedestrians are alpha
+
+    x1 = np.linspace(-0.5, 1.5, nx, dtype=np.float32)
+    x2 = np.linspace(-0.75, 0.75, ny, dtype=np.float32)
+    xx1, xx2 = np.meshgrid(x1, x2)
+    r_ab_probe = torch.from_numpy(np.stack((xx1, xx2), axis=-1)).view(-1, 2).unsqueeze(1)
+
+    speeds_b = torch.ones((r_ab_probe.shape[1],)) * 1.0
+    desired_directions_b = torch.zeros((r_ab_probe.shape[1], 2))
+    desired_directions_b[:, 0] = 1.0
+
+    ax.axhline(0.0, ls='dotted', color='gray')
+    ax.axvline(0.0, ls='dotted', color='gray')
+
+    with torch.no_grad():
+        grad = V.grad_r_ab_(r_ab_probe, speeds_b, desired_directions_b, 1.0)
+        values = torch.linalg.norm(grad, ord=2, dim=-1)
+        values -= torch.min(values)
+    values = values.view((len(x1), len(x2)))
+
+    ax.clabel(
+        ax.contour(x1, x2, values, levels=np.linspace(0, 3.0, 16), vmax=1.5),
+        inline=1, fontsize=10)
+
+    ax.plot([0.0], [0.0], 'o', label='pedestrian', markersize=5.0, color='black')
+    ax.set_xlabel('$x_1$ [m]')
+    ax.set_ylabel('$x_2$ [m]')
+    ax.set_aspect('equal')
+    ax.legend()
