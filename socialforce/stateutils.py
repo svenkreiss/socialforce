accaa5254eb60343d32f183713fd2725e1764921
Sven Kreiss <me@svenkreiss.com>
2021-03-12 08:00:40 +0100

    torch.norm to torch.linalg.norm

--- socialforce/stateutils.py
+++ socialforce/stateutils.py
@@ -6,11 +6,11 @@ import torch
 def desired_directions(state):
     """Given the current state and destination, compute desired direction."""
     destination_vectors = state[:, 6:8] - state[:, 0:2]
-    norm_factors = torch.norm(destination_vectors, dim=-1)
+    norm_factors = torch.linalg.norm(destination_vectors, ord=2, dim=-1)
     norm_factors[norm_factors == 0.0] = 1.0
     return destination_vectors / norm_factors.unsqueeze(-1)
 
 
 def speeds(state):
     """Return the speeds corresponding to a given state."""
-    return torch.norm(state[:, 2:4], dim=-1)
+    return torch.linalg.norm(state[:, 2:4], ord=2, dim=-1)
