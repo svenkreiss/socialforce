fc873369747dd41b1c2afbb5e317fe011aca1df2
Sven Kreiss <me@svenkreiss.com>
2021-09-23 15:25:08 +0200

    nbval dependency

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
             'jupyter-book',
             'matplotlib',
             'nbstripout',
+            'nbval',
             'pycodestyle',
             'pylint~=2.10',
             'pytest',
